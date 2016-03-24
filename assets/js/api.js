34bb7b63c4cdf29546c478489d2e772f2fc10aa4
Kenneth Yee <Kenneth_yee2@yahoo.com>
2016-03-24 18:57:31 -0400

    response data for title/artist/audio_clip in logs

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,3 @@
-// var queryURL = 'https://api.spotify.com/v1/search?q=%22guitar%20madness%22&type=track';
 var baseURL = 'https://api.spotify.com/v1/search?q=';
 
 function makeRequest(songValue){
@@ -6,11 +5,15 @@ function makeRequest(songValue){
 
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response){
 		alert('done!');
+		//response.items[itterate]
+		for (var i = 0; i < response.tracks.items.length; i++) {
+			console.log(response.tracks.items[i]);
+			console.log(response.tracks.items[i].artists[0].name);
+			console.log(response.tracks.items[i].name)
+			console.log(response.tracks.items[i].preview_url);
+		}
+		console.log(queryURL);
 		console.log(response);
-		console.log(response.tracks.items[0].name);
-		console.log(response.tracks.items[0].artists[0].name);
-		console.log(response.tracks.items[0].preview_url);
-
 	});
 	}
 
@@ -21,6 +24,12 @@ $('#search').on('click', function(){
 
 	return false;
 });
+
+// mySound = new Audio(['https://p.scdn.co/mp3-preview/f59a6b5f638815d9116c84d21d8bbf01ffda0892']);
+
+// function playAudio(){
+// 	mySound.play();
+// }
 
 
 
